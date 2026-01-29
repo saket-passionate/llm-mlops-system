d821dd8a13480be8dff58eedb4904583cad1507c
Saket Thavanani <saketthavanani@MacBook-Air-5.local>
2026-01-29 13:10:18 -0500

    gradio

--- app/app.py
+++ app/app.py
@@ -90,4 +90,4 @@ with gr.Blocks(
 # Launch
 # ===============================
 if __name__ == "__main__":
-    demo.launch(server_name='0.0.0.0', server_port='7860')
+    demo.launch(server_name='0.0.0.0', server_port=7860)
